d83f3dda4f9117e021a95f125282e87f8269701f
zlx <248939719@qq.com>
2021-09-15 15:32:42 +0800

    第一版

---
+++ src/main/java/org/zlx/App.java
@@ -0,0 +1,13 @@
+package org.zlx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Wbghgkjhkjhkjorld!" );
+    }
+}
